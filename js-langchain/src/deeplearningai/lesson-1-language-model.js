30d2d90241fa89e22abb6db63a6cd600a07ff7f3
Indra Basak <indra.basak@autodesk.com>
2024-09-18 17:15:24 -0700

    aded lessons 1, 2, and 3 from deeplearningai langchainjs

---
+++ js-langchain/src/deeplearningai/lesson-1-language-model.js
@@ -0,0 +1,31 @@
+const { DefaultAzureCredential, getBearerTokenProvider } = require('@azure/identity');
+const { AzureChatOpenAI } = require('@langchain/openai');
+const { HumanMessage } = require('@langchain/core/messages');
+require('dotenv').config();
+
+async function main() {
+    console.log('== Lesson 1 - Language Model Example ==');
+
+    const credential = new DefaultAzureCredential();
+    const scope = 'https://cognitiveservices.azure.com/.default';
+    const azureADTokenProvider = getBearerTokenProvider(credential, scope);
+
+    const model = new AzureChatOpenAI({
+        azureADTokenProvider,
+        azureOpenAIApiInstanceName:  process.env.AZURE_OPENAI_API_INSTANCE_NAME,
+        azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
+        azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
+    });
+
+    const result = await model.invoke([
+        new HumanMessage('Tell me a joke.')
+    ]);
+
+    console.log(result);
+}
+
+main().catch((err) => {
+    console.error('The sample encountered an error:', err);
+});
+
+module.exports = { main };
