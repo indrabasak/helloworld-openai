0306f90be63db02585144c46b5a5dd38b47be1bf
Indra Basak <indra.basak@autodesk.com>
2024-10-13 21:45:22 -0700

    second revision of csv processor

--- js-langchain/src/processors/csv-processor.js
+++ js-langchain/src/processors/csv-processor.js
@@ -1,11 +1,22 @@
+/**
+ * This is a working example of processing a CSV file.
+ * This example is based on LLM + Spreadsheets
+ * https://www.neum.ai/post/llm-spreadsheets
+ *
+ * @author Indra Basak
+ * @since Oct 12, 2024
+ */
 const fs = require('fs');
 const csv = require('csv-parser');
 const { Document } = require('langchain/document');
 const { Transform } = require('stream');
 const { CharacterTextSplitter } = require('@langchain/textsplitters');
 const { ClientSecretCredential, getBearerTokenProvider } = require('@azure/identity');
-const { AzureOpenAIEmbeddings } = require('@langchain/openai');
+const { AzureOpenAIEmbeddings, AzureChatOpenAI } = require('@langchain/openai');
 const { MemoryVectorStore } = require('langchain/vectorstores/memory');
+const { AttributeInfo } = require('langchain/chains/query_constructor');
+const { SelfQueryRetriever } = require('langchain/retrievers/self_query');
+const { FunctionalTranslator } = require('@langchain/core/structured_query');
 require('dotenv').config();
 
 class StripBomStream extends Transform {
@@ -30,7 +41,7 @@ class StripBomStream extends Transform {
   }
 }
 
-async function main() {
+async function getDocuments() {
   // define the columns we want to embed vs which ones we want in metadata
   const columnsToEmbed = ['Description', 'Features'];
   const columnsToMetadata = ['Product Name', 'Price', 'Rating', 'Description', 'Features'];
@@ -65,78 +76,18 @@ async function main() {
         let newDoc = new Document({ pageContent: toEmbed, metadata: toMetadata });
         // console.log(newDoc);
         docs.push(newDoc);
-        // console.log('----' + docs.length);
       }
     }
   } catch (error) {
     console.error('Error reading CSV file:', error);
   }
-  // const docs= new Promise((resolve, reject) => {
-  //   const docsArray = [];
-  //   fs.createReadStream('./data/TestListings.csv', { encoding: 'utf8' })
-  //     .pipe(new StripBomStream())
-  //     .pipe(csv({ skip_empty_lines: true }))
-  //     .on('data', (row) => {
-  //       // console.log(row); // Process each row of data here
-  //       if (Object.keys(row).length !== 0) {
-  //         const toMetadata = {};
-  //         for (const col of columnsToMetadata) {
-  //           toMetadata[col] = row[col];
-  //         }
-  //
-  //         const valuesToEmbed = {};
-  //         for (const col of columnsToEmbed) {
-  //           valuesToEmbed[col] = row[col];
-  //         }
-  //         // console.log(valuesToEmbed);
-  //
-  //         let toEmbed = Object.entries(valuesToEmbed)
-  //           .map(([key, value]) => `${key.trim()}: ${value.trim()}`)
-  //           .join('\n');
-  //         // console.log(toEmbed);
-  //         let newDoc = new Document({ pageContent: toEmbed, metadata: toMetadata});
-  //         // console.log(newDoc);
-  //         docsArray.push(newDoc);
-  //         console.log('----' + docs.length);
-  //       }
-  //     })
-  //     .on('end', () => {
-  //       resolve(docsArray);
-  //       console.log('CSV file successfully processed');
-  //     });
-  // });
-  // fs.createReadStream('./data/TestListings.csv', { encoding: 'utf8' })
-  // .pipe(new StripBomStream())
-  // .pipe(csv({ skip_empty_lines: true }))
-  // .on('data', (row) => {
-  //   // console.log(row); // Process each row of data here
-  //   if (Object.keys(row).length !== 0) {
-  //     const toMetadata = {};
-  //     for (const col of columnsToMetadata) {
-  //       toMetadata[col] = row[col];
-  //     }
-  //
-  //     const valuesToEmbed = {};
-  //     for (const col of columnsToEmbed) {
-  //       valuesToEmbed[col] = row[col];
-  //     }
-  //     // console.log(valuesToEmbed);
-  //
-  //     let toEmbed = Object.entries(valuesToEmbed)
-  //       .map(([key, value]) => `${key.trim()}: ${value.trim()}`)
-  //       .join('\n');
-  //     // console.log(toEmbed);
-  //     let newDoc = new Document({ pageContent: toEmbed, metadata: toMetadata});
-  //     // console.log(newDoc);
-  //     docs.push(newDoc);
-  //     console.log('----' + docs.length);
-  //   }
-  // })
-  // .on('end', () => {
-  //   console.log('CSV file successfully processed');
-  // });
 
-  // console.log(docs);
+  return docs;
+}
+
+async function main() {
+  console.log('== Example of processing a csv file ==');
+  const docs = await getDocuments();
   // split a document using character splitting
   const splitter = new CharacterTextSplitter({
     separator: '\n',
@@ -145,7 +96,6 @@ async function main() {
     // lengthFunction: len // len is not defined
   });
   const splitDocs = await splitter.splitDocuments(docs);
-  // console.log(splitDocs);
 
   const credential =
     new ClientSecretCredential(process.env.AZURE_TENANT_ID,
@@ -177,6 +127,63 @@ async function main() {
 
   console.log(similaritySearchResults[0].pageContent);
   console.log(similaritySearchResults[0].metadata);
+
+  // Advanced Querying
+  // Metadata schema based on the values on the CSV
+  const attributeInfo = [
+    new AttributeInfo('Product Name', 'string', 'Name of the product'),
+    new AttributeInfo ('Price', 'string', 'The price of the product as a number. Ex. 149.99'),
+    new AttributeInfo('Rating', 'string', 'The rating of the product as a number from 0 to 5. Ex. 4.5'),
+    new AttributeInfo('Description', 'string', 'Description of the product'),
+    new AttributeInfo('Features', 'string', 'Features of the product'),
+  ];
+  // const attributeInfo = [
+  //   {
+  //     name: 'Product Name',
+  //     description: 'Name of the product',
+  //     type:'string',
+  //   },
+  //   {
+  //     name:'Price',
+  //     description:'The price of the product as a number. Ex. 149.99',
+  //     type:'string',
+  //   },
+  //   {
+  //     name: 'Rating',
+  //     description: 'The rating of the product as a number from 0 to 5. Ex. 4.5',
+  //     type: 'string',
+  //   },
+  //   {
+  //     name: 'Description',
+  //     description: 'Description of the product',
+  //     type: 'string'
+  //   },
+  //   {
+  //     name: 'Features',
+  //     description: 'Features of the product',
+  //     type: 'string'
+  //   },
+  // ];
+  const documentContents = 'Product listing';
+
+  const llm = new AzureChatOpenAI({
+    azureADTokenProvider,
+    azureOpenAIApiInstanceName:  process.env.AZURE_OPENAI_API_INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
+    azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
+    temperature: 0
+  });
+
+  const retriever = SelfQueryRetriever.fromLLM({
+    llm,
+    vectorStore,
+    documentContentDescription: documentContents,
+    attributeInfo,
+    structuredQueryTranslator: new FunctionalTranslator(),
+  });
+
+  await retriever.invoke('good heart monitor');
+
 }
 
 main().catch((err) => {
